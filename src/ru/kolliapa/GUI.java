13efdc3db8d22b0af099595f539f479fc85bafc6
kolliapa <across-kolliapa@mail.ru>
2017-09-28 15:58:11 +0300

    'The_Game_Project'

---
+++ src/ru/kolliapa/GUI.java
@@ -0,0 +1,7 @@
+package ru.kolliapa;
+
+/**
+ * Created by PC-01 on 28.09.2017.
+ */
+public class GUI {
+}
---
+++ src/ru/kolliapa/main.java
@@ -0,0 +1,10 @@
+package ru.kolliapa;
+
+/**
+ * Created by PC-01 on 28.09.2017.
+ */
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
